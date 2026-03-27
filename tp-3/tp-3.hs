e6d042218f1adc4957cc86e1ef3f73b02aa55f28
MateoGiuffra <matteogiuffrah40@gmail.com>
2026-03-27 16:22:30 -0300

    arranco ej 6 del tp3

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -5,6 +5,12 @@ swap (x, y) = (y, x)
 doble :: Int -> Int
 doble = (* 2)
 
+fst :: (a,b) -> a
+fst (x, y) = x 
+
+snd :: (a,b) -> b
+snd (x, y) = y
+
 -- DEFINICIONES -- DEFINICIONES -- DEFINICIONES -- DEFINICIONES
 
 -- ej1
@@ -51,33 +57,168 @@ compose f g x = f (g x)
 {-
 
 -- ej 4
+Regla de aplicación:
+f   :: A -> B
+e   :: A
+f e :: B
 
 a. apply apply apply
-((apply apply) apply) 
+((apply apply) apply)
 def de apply: f x = f x, donde f = apply
 -> \apply -> \x -> apply x
 por red beta
 -> (\x -> apply x) apply
 por def de apply, donde x (que seria la funcion f) es el ultimo apply
--> (\apply -> apply apply) 
+-> (\apply -> apply apply)
 por red beta
--> apply 
+-> apply
 
-el resultado de aplicar apply 3 veces consecutivas nos da apply, 
+el resultado de aplicar apply 3 veces consecutivas nos da apply,
 cuyo tipado es = (a -> b) -> a -> b
 por lo tanto
 apply apply apply :: (a -> b) -> a -> b
 
-
 b. twice doble 2
 ((twice doble) 2)
 
+twice       :: (Int -> Int) -> (Int -> Int)
+doble       :: (Int -> Int)
+---------------------------
+twice doble :: (Int -> Int)         (twice doble)   :: Int -> Int
+                                    2               :: Int
+                                    ------------------------------
+                                    (twice doble) 2 :: Int
+
 c. twice twice twice swap
 (((twice twice) twice) swap)
+twice       :: (a1 -> a1) -> (a1 -> a1)
+twice       :: (a2 -> a2) -> (a2 -> a2)
+---------------------
+twice twice :: B
+
+ ---------> a1 = (a2 -> a2)
+
+twice       :: ((a2 -> a2) -> (a2 -> a2)) -> ((a2 -> a2) -> (a2 -> a2))
+twice       :: (a2 -> a2) -> (a2 -> a2)
+---------------------
+twice twice :: (a2 -> a2) -> (a2 -> a2)     twice twice       :: (a2 -> a2) -> (a2 -> a2)
+                                            twice             :: (a3 -> a3) -> (a3 -> a3)
+                                            ---------------------
+                                            (twice twice) twice :: B'
+
+                                             ---------> a2 = (a3 -> a3)
+
+                                            twice twice       :: ((a3 -> a3) -> (a3 -> a3)) -> ((a3 -> a3) -> (a3 -> a3))
+                                            twice             :: (a3 -> a3) -> (a3 -> a3)
+                                            ---------------------
+                                            (twice twice) twice :: (a3 -> a3) -> (a3 -> a3)     (twice twice) twice         :: (a3 -> a3) -> (a3 -> a3)
+                                                                                                swap                        :: (b, c) -> (c, b)
+                                                                                                -------------------------------------
+                                                                                                ((twice twice) twice) swap  :: B''
+
+                                                                                                 ---------> (a3 -> a3) = (b, c)
+                                                                                                 --------->  (a3 -> a3) = (c, b)
+                                                                                                 ---------> esto es posible solamente si b = c. 
+                                                                                                 ---------> Renombro la igualdad usando la letra d. b = c = d. 
+
+                                                                                                (twice twice) twice         :: ((d,d) -> (d,d)) -> ((d,d) -> (d,d))
+                                                                                                swap                        :: (d, d) -> (d, d)
+                                                                                                -------------------------------------
+                                                                                                ((twice twice) twice) swap  :: (d,d) -> (d,d)
 
 d. flip twice 1 doble
 (((flip twice) 1) doble)
+flip        :: (b -> a -> c) -> a -> b -> c
+twice       :: (d -> d) -> (d -> d)
+-------------------------------
+flip twice  :: 
+(b       -> a -> c) 
+         =
+(d -> d) -> (d -> d)
+ --------> b        = (d -> d)
+ --------> (a -> c) = (d -> d)
+ --------> esto implica que a = d, c = d
+
+flip        :: ((d -> d) -> d -> d) -> d -> (d -> d) -> d
+twice       :: (d -> d) -> (d -> d)
+-----------------------------------------------------------
+flip twice  :: d -> (d -> d) -> d               
 
 
+(flip twice)    :: d -> (d -> d) -> d
+1               :: Int
+----------------------------------------
+(flip twice) 1  :: 
+
+ ---------> d = Int
+
+(flip twice)    :: Int -> ((Int -> Int) -> Int)
+1               :: Int
+----------------------------------------
+(flip twice) 1  :: (Int -> Int) -> Int
+
+ --------->
+
+(flip twice) 1          ::  (Int -> Int) -> Int  
+doble                   ::  Int -> Int
+-------------------------------------------------------
+((flip twice) 1) doble  :: Int
+
+
+Ejercicio 5
+
+a. appDup f x = f (x, x) 
+b. appFork (f, g) x = (f x, g x) 
+c. appPar​ ​(f, g) (x, y) = (f x, g y) 
+d. appDist f (x, y) = (f x, f y) 
+e. subst f g x = (f x) (g x)
+
+
+Ejercicio 6
+
+a. compose (fst snd)
+compose 
+fst snd
+-------------------
+compose (fst snd)
+
+
+fst     :: (a, b) -> a
+snd     :: (c, d) -> d
+---------------------
+fst snd :: 
+
+(a, b)
+(c, d) -> d
+
+fst espera una tupla, pero snd es de tipo funcion que devuelve un valor.
+En este caso habria error.
+
+variante: compose fst snd
+
+b. ((uncurry curry) snd)
+uncurry       :: (a -> b -> c) -> ((a,b) -> c)
+curry         :: ((d,e) -> f) -> (d -> e -> f)
+----------------------------------------
+uncurry curry :: 
+
+
+(a           -> (b -> c))
+              = 
+((d,e) -> f) -> (d -> e -> f) 
+
+a        = (d, e) -> f
+(b -> c) = (d -> e -> f) 
+
+(b      -> c) 
+    = 
+(d      -> (e -> f)) 
+
+
+
+uncurry       :: ((d -> (e -> f)) -> d -> (e -> f)) -> (((d -> c), d) -> (e -> f))
+curry         :: ((d,e) -> f) -> (d -> e -> f)
+----------------------------------------
+uncurry curry :: ((d -> c), d) -> (e -> f)
 
 -}
