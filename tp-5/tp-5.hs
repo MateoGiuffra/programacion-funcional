4fdbe58629599984fdd7046cd1216c8d335ae1e8
MateoGiuffra <matteogiuffrah40@gmail.com>
2026-04-05 00:28:13 -0300

    termino tp-5

--- tp-5/tp-5.hs
+++ tp-5/tp-5.hs
@@ -1,4 +1,23 @@
 import Prelude hiding (uncurry)
+-- definiciones
+
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f x y = f (x, y)
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (x, y) = f x y
+
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g x = f (g x)
+
+flip :: (b -> a -> c) -> a -> b -> c
+flip f x y = (f y) x
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+-- definiciones
+
 
 data Gusto = Chocolate | DulceDeLeche | Frutilla | Sambayon
   deriving (Show)
@@ -138,191 +157,60 @@ asFoot (Inch x) = Foot (x * 0.083)
 asFoot medida = medida
 
 -- ej 5
--- definiciones
-curry :: ((a, b) -> c) -> a -> b -> c
-curry f x y = f (x, y)
-
-uncurry :: (a -> b -> c) -> (a, b) -> c
-uncurry f (x, y) = f x y
-
-compose :: (b -> c) -> (a -> b) -> a -> c
-compose f g x = f (g x)
-
-flip :: (b -> a -> c) -> a -> b -> c
-flip f x y = (f y) x
-
-swap :: (a, b) -> (b, a)
-swap (x, y) = (y, x)
-
--- definiciones
-
 data Shape = Circle Float | Rect Float Float
   deriving (Show)
 
 construyeShNormal :: (Float -> Shape) -> Shape
 construyeShNormal c = c 1.0
+-- LAS JUSTIFICACIONES ESTAN EN EL ARCHIVO ej-5.hs
 
--- uncurry Rect :: (Float, Float) -> Shape
---  construyeShNormal (flip Rect 5.0) :: Shape
--- por def de construyeShNormal, c <- (flip Rect 5.0)
--- (flip Rect 5.0) 1.0
--- por def de flip, f <- Rect, x <- 5.0, y <- 1.0
--- (Rect 1.0) 5.0
--- por lo tanto el tipo es Shape
+-- a. uncurry Rect :: (Float, Float) -> Shape 
+-- b. construyeShNormal (flip Rect 5.0) :: Shape 
+-- c. compose (uncurry Rect) swap :: (Float, Float) -> Shape 
+-- d. uncurry Cucurucho :: (Gusto, Gusto) -> Helado 
+-- e. uncurry Rect swap :: No tipa (Error de tipos) 
+-- f. compose uncurry Pote :: Gusto -> (Gusto, Gusto) -> Helado 
+-- g. compose Just :: (a -> b) -> a -> Maybe b 
+-- h. compose uncurry (Pote Chocolate) :: No tipa (Error de tipos)
 
--- compose (uncurry Rect) swap ::
-{-
-(compose (uncurry Rect)) swap
-
-###############1###############
-uncurry         :: (a -> b -> c) -> (a, b) -> c
-Rect            :: Float -> Float -> Shape
--------------------------------
-(uncurry Rect)  :: ??
-
-Float -> Float -> Shape
-(a -> b -> c
- -----> a = Float
- -----> b = Float
- -----> c = Shape
-
-uncurry         :: (Float -> Float -> Shape) -> (Float, Float) -> Shape
-Rect            :: Float -> Float -> Shape
--------------------------------
-(uncurry Rect)  :: (Float, Float) -> Shape
-
-###############1###############
-
-###############2###############
-compose         :: (e -> f) -> (d -> e) -> d -> f
-(uncurry Rect)  :: (Float, Float) -> Shape
--------------------------------
-compose (uncurry Rect) :: ???
-
- -----> e = (Float, Float)
- -----> f = Shape
-
-compose         :: ((Float, Float) -> Shape) -> (d -> (Float, Float)) -> d -> Shape
-(uncurry Rect)  :: (Float, Float) -> Shape
--------------------------------
-compose (uncurry Rect) :: (d -> (Float, Float)) -> d -> Shape
-
-###############2###############
-
-###############3###############
-compose (uncurry Rect) :: (d -> (Float, Float)) -> d -> Shape
-swap  :: (x, y) -> (y, x)
--------------------------------
-compose (uncurry Rect) swap :: ??
-
-(x, y) -> (y, x)
-(d -> (Float, Float))
-
-d = (x, y)
-(y, x) = (Float, Float)
-por lo tanto
-y = Float
-x = Float
-
-d = (Float, Float)
-
-compose (uncurry Rect) :: ((Float, Float) -> (Float, Float)) ->(Float, Float) -> Shape
-swap  :: (x, y) -> (y, x)
--------------------------------
-compose (uncurry Rect) swap :: (Float, Float) -> Shape
-
-###############3###############
-
--}
-
--- uncurry Cucurucho ::  (Gusto, Gusto) -> Helado
--- uncurry Rect swap :: No tipa
-{-
-Rompe porque uncurry Rect espera una tupla y swap es una funcion
--}
--- compose uncurry Pote :: Gusto -> (Gusto, Gusto) -> Helado
-{-
-###############1###############
-compose :: (b -> c) -> (a -> b) -> a -> c
-uncurry :: (d -> e -> f) -> (d, e) -> f
-compose uncurry ::
-
-(d -> e -> f) -> ((d, e) -> f)
-(b -> c)
-
- -----> b = (d -> e -> f)
- -----> c = ((d, e) -> f)
-
-compose :: ((d -> e -> f) -> ((d, e) -> f)) -> (a -> (d -> e -> f)) -> a -> ((d, e) -> f)
-
-uncurry :: (d -> e -> f) -> (d, e) -> f
-compose uncurry :: (a -> d -> e -> f) -> a -> (d, e) -> f
-
-###############1###############
-
-###############2###############
-compose uncurry       :: (a -> d -> e -> f) -> a -> (d, e) -> f
-Pote                  :: Gusto -> Gusto -> Gusto -> Helado
----------------------------------------------------------------
-compose uncurry Pote  :: ??
-
-Gusto -> Gusto -> Gusto -> Helado
-(a    -> d    -> e      -> f)
- -----> a = Gusto
- -----> d = Gusto
- -----> e = Gusto
- -----> f = Helado
-
-compose uncurry       :: (Gusto -> Gusto -> Gusto -> Helado) -> Gusto -> (Gusto, Gusto) -> Helado
-Pote                  :: Gusto -> Gusto -> Gusto -> Helado
----------------------------------------------------------------
-compose uncurry Pote  :: Gusto -> (Gusto, Gusto) -> Helado
-
-###############2###############
--}
-
--- compose Just :: (a -> b) -> a -> Maybe b
--- compose uncurry (Pote Chocolate) ::
-{-
-###############1###############
-compose           :: (b -> c) -> (a -> b) -> a -> c
-uncurry           :: (d -> e -> f) -> (d, e) -> f
--------------------------------
-compose uncurry   :: ???
+-- ej 6
+-- uncurry Rect (1.0, 1.0)
+-- compose (uncurry Rect) swap (1.0, 1.0)
+-- compose uncurry Pote Chocolate (Vainilla, Vainilla)
+-- compose Just doble 2 
 
 
-(b -> c)
-(d -> e -> f) -> ((d, e) -> f)
+-- ej 7
+data Set a = S (a -> Bool)
 
- --------> b = (d -> e -> f)
- --------> c = ((d, e) -> f)
+belongs :: Set a -> a -> Bool
+-- que dado un conjunto, describe la función que indica si un elemento dado pertenece a ese conjunto.
+belongs (S f) x = f x
 
+empty :: Set a
+-- que describe el conjunto vacío
+empty = (S (\x -> False))
 
-compose           :: ((d -> e -> f) -> ((d, e) -> f)) -> (a -> (d -> e -> f)) -> a -> ((d, e) -> f)
-uncurry           :: (d -> e -> f) -> (d, e) -> f
--------------------------------
-compose uncurry   :: (a -> d -> e -> f) -> a -> (d, e) -> f
-###############1###############
+singleton :: (Eq a) => a -> Set a
+-- que dado un elemento describe un conjunto que contiene a ese único elemento
+singleton x = (S (\y -> x == y))
 
+union :: Set a -> Set a -> Set a
+-- que dados dos conjuntos, describe al conjunto que resulta de la unión de ambos
+union (S f) (S g) = S (\x -> f x || g x)
 
-###############2###############
-compose uncurry                   :: (a -> d -> e -> f) -> a -> (d, e) -> f
-(Pote Chocolate)                  :: Gusto -> Gusto -> Helado
--------------------------------
-compose uncurry (Pote Chocolate)  :: ???
-
-(a -> d -> (e -> f))
-Gusto -> Gusto -> Helado
-
-a = Gusto
-d = Gusto
-e -> f = Helado
+intersection :: Set a -> Set a -> Set a
+-- que dados dos conjuntos, describe al conjunto que resulta de la intersección de ambos.
+intersection (S f) (S g) = S (\x -> f x && g x)
 
 
-Helado es una expresion atomica, no una funcion. Por lo tanto, error de tipo.
+-- ej 8
+data Exception = DivByZero | NotFound | NullPointer | Other String
+data MayFail a = Raise Exception | Ok a
+type ExHandler a  = Exception -> a
 
-falta un parametro? o en realidad esto (a -> d -> e -> f) dice dame un Gusto, otro gusto, un helado y te doy f, no sabemos
-que es f pero te lo doy?
-###############2###############
+tryCatch :: MayFail a -> (a -> b) -> ExHandler b -> b
+tryCatch (Ok a) f _ = f a
+tryCatch (Raise exception) f handler = handler exception
 
--}
+
