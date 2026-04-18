02834d14750ba4845d8c574842dba6025e00de35
MateoGiuffra <matteogiuffrah40@gmail.com>
2026-04-18 12:27:49 -0300

    agrego resolucion de los ejercicios de la clase del 16/04

---
+++ ejercicios-extras/16-04/EJERCICIOS.md
@@ -0,0 +1,17 @@
+# Ejercicios: Combinadores y Composición
+
+1. Establecer el tipo de:
+   `(.) (.) (.)`
+
+2. Dar una expresión lambda equivalente que **NO** use `(.)` (ni `compose`).
+   *Sugerencia: completar con parámetros y usarlos, usando el ejercicio anterior como guía, y reducir el cuerpo de la función resultante.*
+
+3. Determinar si:
+   `(.) (.) (.) succ div`
+   es parcial o total.
+
+4. Determinar si:
+   `(.) (.) (.)`
+   es parcial o total.
+
+5. Determinar si yTambien es estricto o no estricto.
--- main.hs
+++ main.hs
@@ -2,8 +2,9 @@ double :: Int -> Int
 double x = x + x
 
 twice :: (a -> a) -> (a -> a)
-twice f = g 
-    where g x = f (f x)
+twice f = g
+  where
+    g x = f (f x)
 
 -- suma x = \y -> x + y
 suma x y = x + y
@@ -11,16 +12,23 @@ suma x y = x + y
 succ = suma 1
 
 swap (x, y) = (y, x)
-sumPar (x,y) = x + y
 
+sumPar (x, y) = x + y
 
 -- uflip f = g where g p = f (swap p)
-
 
 uflip f = \p -> f (swap p)
 
 -- appDup f x = f(x,x)
 appDup f = g where g x = f (x, x)
 
-first :: (a,b) -> a
-first (a,b) = a
+first :: (a, b) -> a
+first (a, b) = a
+
+f x = if x == x then x else error "te cague"
+
+yTambien :: Bool -> Bool -> Bool
+yTambien True x = x
+yTambien False _ = False
+yTambien _ _ = False
+
