16030e437252c84f5f0869afa1aaab998807667f
MateoGiuffra <matteogiuffrah40@gmail.com>
2026-05-07 22:53:09 -0300

    avance de tp6

--- tp-6/tp-6.hs
+++ tp-6/tp-6.hs
@@ -68,3 +68,55 @@ curry f a b = f (a, b)
 
 uncurry :: (a -> b -> c) -> (a, b) -> c
 uncurry f (a, b) = f a b
+
+{-
+ej 4
+a.
+curry fst = const
+
+para todo x, y
+    curry fst x y = const x y
+
+siendo a, b unos valores cualquiera
+
+    lado izq: 
+    curry fst a b
+    = (def curry, f <- fst, a <- a, b <- b)
+    fst (a, b)
+    = (def fst, x <- a)
+    a
+
+    lado der:
+    const a b
+    = (def const, x <- a)
+    a
+
+b.
+
+--- def ---
+flip :: (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+--- def ---
+
+uncurry (flip const) = snd
+
+para todo (x, y)
+    uncurry (flip const) (x, y) = snd (x, y)
+
+siendo (a, b) un par cualquiera
+    lado izq:
+    uncurry (flip const) (a, b)
+    = (def uncurry, <- flip const, (a, b) <- (a, b))
+    flip const a b
+    = (def flip, f <- const, x <- a, y <- b)
+    const b a
+    = (const, x <- b)
+    b
+
+    lado der:
+    snd (a, b)
+    = (def snd, y <- b)
+    b
+-}
+
+
